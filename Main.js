12170dd53a89578c3c4ca0051e9201f2afd6b9ea
Eriberto <ebailon0319@gmail.com>
2021-06-01 12:57:18 -0700

    added js, html, and css files

--- Main.js
+++ Main.js
@@ -1,26 +1,3 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    getCar()
-    showAllVehicles()
+    
 })
-
-const getCar = () => {
-    fetch('https://swapi.dev/api/vehicles/')
-    .then(res => res.json())
-    .then(data => data.forEach(renderVehicleList))
-    console.log('fetch fetched')
-}
-
-//Button function to call all vehicles
-const showAllVehicles = () => {
-    const bttn = document.getElementById('car-list')
-    bttn.addEventListener("click", (e) => {
-        e.preventDefault()
-        console.log('test')
-    })
-}
-
-//render function to an li
-const renderVehicleList = () => {
-    let li = document.querySelector('ul').createElement('li')
-    li.append("test")
-}
